09ec79a6fbac91990b8f5b7af500f3ff5801df5e
ratherbot <just51290@mail.ru>
2026-02-10 03:00:17 +0300

    lst

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
+import os
 from datetime import datetime
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-DATABASE_URL = "postgresql+asyncpg://postgres:your_password@localhost:5432/lapp_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:your_password@localhost:5432/lapp_db")
 
 engine = create_async_engine(DATABASE_URL)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
